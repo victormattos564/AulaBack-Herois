cb8f78b628e83f5ea8956c3ada67d1ac3bcbde01
iam.mattosz <victormaria@aluno.senai.br>
2024-05-06 11:52:52 -0300

    Atualizar tabela de batalhas para incluir o campo "perdedor_id"

--- db/tables-battles.sql
+++ db/tables-battles.sql
@@ -1,22 +1,22 @@
 CREATE TABLE battles (
-    id SERIAL PRIMARY KEY NOT NULL,
-    winner_name INT NOT NULL,
-    hero1_id INT NOT NULL,
-    hero2_id INT NOT NULL,
+    id SERIAL PRIMARY KEY,
+    winner_id INTEGER,
+    hero1_id INTEGER NOT NULL,
+    hero2_id INTEGER NOT NULL,
+    perdedor_id INTEGER,
     FOREIGN KEY (hero1_id) REFERENCES heroes(id),
     FOREIGN KEY (hero2_id) REFERENCES heroes(id),
-    FOREIGN KEY (winner_name) REFERENCES heroes(id)
+    FOREIGN KEY (winner_id) REFERENCES heroes(id)
 );
+  
 
-
-
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (1, 1, 2);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (2, 3, 4);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (3, 5, 6);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (4, 7, 8);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (5, 9, 10);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (6, 11, 12);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (7, 13, 14);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (8, 15, 16);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (9, 17, 18);
-INSERT INTO battles (winner_name, hero1_id, hero2_id) VALUES (10, 19, 20);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (1, 1, 2);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (2, 1, 3);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (3, 1, 4);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (4, 1, 5);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (5, 1, 6);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (6, 1, 7);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (7, 1, 8);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (8, 1, 9);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (9, 1, 10);
+INSERT INTO battles (winner_id, hero1_id, hero2_id) VALUES (10, 1, 11);
